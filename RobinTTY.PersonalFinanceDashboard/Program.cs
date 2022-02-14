3b56893c90917208f269c776c8df45cad2ed8f96
RobinTTY <muellerobin95@gmail.com>
2022-02-14 19:21:39 +0100

    Add some testing code

--- RobinTTY.PersonalFinanceDashboard/Program.cs
+++ RobinTTY.PersonalFinanceDashboard/Program.cs
@@ -1,1 +1,4 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using RobinTTY.PersonalFinanceDashboard.DataImport.Etoro;
+
+var importer = new EtoroImporter();
+importer.ImportAccountStatement(@"C:\Users\Robin\Downloads\etoro-account-statement-1-1-2022-2-12-2022 (1).xlsx");
