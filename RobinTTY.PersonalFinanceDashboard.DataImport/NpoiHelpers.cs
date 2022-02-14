d299b5e49762d16992db6c27fe51a45ba0336ecb
RobinTTY <muellerobin95@gmail.com>
2022-02-14 19:24:01 +0100

    Add NpoiHelpers class

---
+++ RobinTTY.PersonalFinanceDashboard.DataImport/NpoiHelpers.cs
@@ -0,0 +1,11 @@
+﻿using NPOI.SS.UserModel;
+
+namespace RobinTTY.PersonalFinanceDashboard.DataImport;
+
+public static class NpoiHelpers
+{
+    public static ICell GetCell(this ISheet workbookSheet, int row, int column)
+    {
+        return workbookSheet.GetRow(row).GetCell(column);
+    }
+}
