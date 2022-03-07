edc2004c8c0710ef561f367f0d3509263e653f89
RobinTTY <muellerobin95@gmail.com>
2022-03-07 16:37:25 +0100

    Add column attributes

--- src/RobinTTY.PersonalFinanceDashboard.DataImport/Etoro/Models/EtoroAccountActivity.cs
+++ src/RobinTTY.PersonalFinanceDashboard.DataImport/Etoro/Models/EtoroAccountActivity.cs
@@ -1,15 +1,26 @@
-﻿namespace RobinTTY.PersonalFinanceDashboard.DataImport.Etoro.Models;
+﻿using Npoi.Mapper.Attributes;
+
+namespace RobinTTY.PersonalFinanceDashboard.DataImport.Etoro.Models;
 
 public class EtoroAccountActivity
 {
+    [Column("Date")]
     public DateTime Date { get; set; }
+    [Column("Type")]
     public EtoroAccountActivityType Type { get; set; }
+    [Column("Details")]
     public string Details { get; set; }
+    [Column("Amount")]
     public decimal Amount { get; set; }
+    [Column("Realized Equity Change")]
     public decimal RealizedEquityChange { get; set; }
+    [Column("Realized Equity")]
     public decimal RealizedEquity { get; set; }
+    [Column("Balance")]
     public decimal Balance { get; set; }
+    [Column("Position ID")]
     public long PositionId { get; set; }
+    [Column("NWA")]
     public decimal Nwd { get; set; }
 }
 
--- src/RobinTTY.PersonalFinanceDashboard.DataImport/Etoro/Models/EtoroDividends.cs
+++ src/RobinTTY.PersonalFinanceDashboard.DataImport/Etoro/Models/EtoroDividends.cs
@@ -1,13 +1,23 @@
-﻿namespace RobinTTY.PersonalFinanceDashboard.DataImport.Etoro.Models;
+﻿using Npoi.Mapper.Attributes;
+
+namespace RobinTTY.PersonalFinanceDashboard.DataImport.Etoro.Models;
 
 public class EtoroDividends
 {
+    [Column("Date of Payment")]
     public DateTime DateOfPayment { get; set; }
+    [Column("Instrument Name")]
     public string InstrumentName { get; set; }
+    [Column("Net Dividend Received (USD)")]
     public decimal NetDividendReceived { get; set; }
+    [Column("Withholding Tax Rate (%)")]
     public ushort WithholdingTaxRate { get; set; }
+    [Column("Withholding Tax Amount (USD)")]
     public decimal WithholdingTaxAmount { get; set; }
+    [Column("Position ID")]
     public long PositionId { get; set; }
+    [Column("Type")]
     public EtoroInstrumentType Type { get; set; }
+    [Column("ISIN")]
     public string Isin { get; set; }
 }
