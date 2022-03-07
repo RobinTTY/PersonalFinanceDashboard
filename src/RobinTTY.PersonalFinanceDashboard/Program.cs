c43ea86cf6366ef77e2c067e5550ef97b807f2b3
RobinTTY <muellerobin95@gmail.com>
2022-03-07 16:52:29 +0100

    Update example usage

--- src/RobinTTY.PersonalFinanceDashboard/Program.cs
+++ src/RobinTTY.PersonalFinanceDashboard/Program.cs
@@ -1,4 +1,4 @@
 ﻿using RobinTTY.PersonalFinanceDashboard.DataImport.Etoro;
 
 var importer = new EtoroImporter();
-importer.ImportAccountStatement(@"C:\Users\Robin\OneDrive\etoro-account-statement-1-1-2022-1-22-2022.xlsx");
+var accountStatement = importer.ImportAccountStatement(@"C:\Users\Robin\Desktop\eToro\etoro-account-statement-1-1-2022-2-12-2022.xlsx");
