17525c0e11b174b6146476d6579698d9f4055f7c
Robin Müller <muellerobin95@gmail.com>
2022-03-22 01:35:37 +0100

    Add documentation template. Resolves #23

---
+++ docs/product/docusaurus.config.js
@@ -0,0 +1,108 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+
+const projectTitle = "Personal Finance Dashboard";
+const repoLink = "https://github.com/RobinTTY/PersonalFinanceDashboard";
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: projectTitle,
+  tagline: "Making personal finance simple.",
+  url: "https://your-docusaurus-test-site.com",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "RobinTTY",
+  projectName: "PersonalFinanceDashboard",
+
+  presets: [
+    [
+      "classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          editUrl: repoLink,
+        },
+        blog: {
+          showReadingTime: true,
+          editUrl: repoLink,
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: projectTitle,
+        logo: {
+          alt: "My Site Logo",
+          src: "img/logo.svg",
+        },
+        items: [
+          {
+            type: "doc",
+            docId: "intro",
+            position: "left",
+            label: "Docs",
+          },
+          { to: "/blog", label: "Blog", position: "left" },
+        ],
+      },
+      footer: {
+        style: "dark",
+        links: [
+          {
+            title: "Learn",
+            items: [
+              {
+                label: "Docs",
+                to: "/docs/intro",
+              },
+            ],
+          },
+          {
+            title: "Socials",
+            items: [
+              {
+                label: "Twitter",
+                href: "https://twitter.com/Robin_tty",
+              },
+              {
+                label: "LinkedIn",
+                href: "https://www.linkedin.com/in/robin-m%C3%BCller-574782170",
+              },
+            ],
+          },
+          {
+            title: "More",
+            items: [
+              {
+                label: "Blog",
+                to: "/blog",
+              },
+              {
+                label: "GitHub",
+                href: "https://github.com/RobinTTY/PersonalFinanceDashboard",
+              },
+            ],
+          },
+        ],
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
