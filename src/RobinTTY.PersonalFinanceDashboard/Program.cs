9e17ad1e7e1c1a7ce78db118c8aff9a1cf9f079a
Robin Müller <muellerobin95@gmail.com>
2022-03-22 01:55:49 +0100

    Deploy website - based on 17525c0e11b174b6146476d6579698d9f4055f7c

--- src/RobinTTY.PersonalFinanceDashboard/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿using RobinTTY.PersonalFinanceDashboard.DataImport.Etoro;
-
-var etoroImporter = new EtoroImporter();
-var accountStatement = etoroImporter.ImportAccountStatement(@"C:\Users\Robin\Desktop\eToro\etoro-account-statement-1-1-2022-3-6-2022.xlsx");
-Console.ReadKey();
