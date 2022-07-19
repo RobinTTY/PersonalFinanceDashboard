fad688c76dbccfe6fcb0e72ef2e03ee14305ee7c
RobinTTY <muellerobin95@gmail.com>
2022-07-19 20:36:22 +0200

    Add initial app config for testing purposes

---
+++ src/RobinTTY.PersonalFinanceDashboard.API/Models/AppConfiguration.cs
@@ -0,0 +1,36 @@
+﻿namespace RobinTTY.PersonalFinanceDashboard.API.Models;
+
+/// <summary>
+/// The configuration of the application server.
+/// </summary>
+public class AppConfiguration
+{
+    public NordigenApiClientConfiguration NordigenApi { get; set; }
+}
+
+/// <summary>
+/// Configuration parameters for the Nordigen API client.
+/// </summary>
+public class NordigenApiClientConfiguration
+{
+    /// <summary>
+    /// The nordigen API endpoint to use.
+    /// </summary>
+    public string Url { get; set; }
+    /// <summary>
+    /// The secret id to access the Nordigen API.
+    /// </summary>
+    public string SecretId { get; set; }
+    /// <summary>
+    /// The secret key to access the Nordigen API.
+    /// </summary>
+    public string SecretKey { get; set; }
+    /// <summary>
+    /// Unclear documentation on this parameter. Default value = 5.
+    /// </summary>
+    public int AccessTokenValidBeforeSeconds { get; set; }
+    /// <summary>
+    /// Unclear documentation on this parameter. Default value = 5.
+    /// </summary>
+    public int RefreshTokenValidBeforeSeconds { get; set; }
+}
---
+++ src/RobinTTY.PersonalFinanceDashboard.API/Utility/AppConfigurationManager.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using RobinTTY.PersonalFinanceDashboard.API.Models;
+
+namespace RobinTTY.PersonalFinanceDashboard.API.Utility;
+
+public class AppConfigurationManager
+{
+    public static IConfiguration GetApplicationConfiguration()
+    {
+        return new ConfigurationBuilder()
+            .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+            .AddJsonFile("appsettings.json", false, true)
+            .AddUserSecrets<AppConfiguration>(false, true)
+            .Build();
+    }
+}
