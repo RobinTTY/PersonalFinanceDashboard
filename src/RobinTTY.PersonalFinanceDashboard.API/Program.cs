e815a465ff2c720ab7f69dc36a83f6df11d9579e
RobinTTY <muellerobin95@gmail.com>
2023-05-01 18:33:37 +0200

    Move Query class, some setup work

--- src/RobinTTY.PersonalFinanceDashboard.API/Program.cs
+++ src/RobinTTY.PersonalFinanceDashboard.API/Program.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Hosting;
 using Autofac.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using RobinTTY.PersonalFinanceDashboard.API.Resolvers;
 using RobinTTY.PersonalFinanceDashboard.API.Utility;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -37,18 +38,3 @@ app.Run();
 
 public record Book(string Title, Author Author);
 public record Author(string Name);
-
-public class Query
-{
-    private readonly List<Book> _books = new()
-    {
-        new Book("C# in Depth: Fourth Edition", new Author("Jon Skeet")),
-        new Book("Learning GraphQL: Declarative Data Fetching for Modern Web Apps", new Author("Eve Porcello and Alex Banks")),
-        new Book("Code: The Hidden Language of Computer Hardware and Software", new Author("Charles Petzold"))
-    };
-
-    public List<Book> GetBooks() => _books;
-    public Book? GetBook(string title) => _books.FirstOrDefault(x => x.Title == title);
-
-    public Author? GetAuthor(string name) => _books.FirstOrDefault(x => x.Author.Name == name)?.Author;
-}
---
+++ src/RobinTTY.PersonalFinanceDashboard.API/Resolvers/Query.cs
@@ -0,0 +1,16 @@
+﻿namespace RobinTTY.PersonalFinanceDashboard.API.Resolvers;
+
+public class Query
+{
+    private readonly List<Book> _books = new()
+    {
+        new Book("C# in Depth: Fourth Edition", new Author("Jon Skeet")),
+        new Book("Learning GraphQL: Declarative Data Fetching for Modern Web Apps", new Author("Eve Porcello and Alex Banks")),
+        new Book("Code: The Hidden Language of Computer Hardware and Software", new Author("Charles Petzold"))
+    };
+
+    public List<Book> GetBooks() => _books;
+    public Book? GetBook(string title) => _books.FirstOrDefault(x => x.Title == title);
+
+    public Author? GetAuthor(string name) => _books.FirstOrDefault(x => x.Author.Name == name)?.Author;
+}
