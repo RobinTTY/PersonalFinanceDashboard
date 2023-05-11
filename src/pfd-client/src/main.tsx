118dcb96cb5ba9810cae19c93b8c5dc2c19acac4
RobinTTY <muellerobin95@gmail.com>
2023-05-11 23:25:50 +0200

    Use Mantine instead of ChakraUI

--- src/pfd-client/src/main.tsx
+++ src/pfd-client/src/main.tsx
@@ -1,17 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
-import theme from "./theme";
-
-const rootElement = document.getElementById("root") as HTMLElement;
-ReactDOM.createRoot(rootElement).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-    <ChakraProvider theme={theme}>
-      <App />
-    </ChakraProvider>
-  </React.StrictMode>
-);
+    <App />
+  </React.StrictMode>,
+)
