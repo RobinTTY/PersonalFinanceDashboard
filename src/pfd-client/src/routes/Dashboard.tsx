88182ac606cf09cc0856995644835e63db00ac09
RobinTTY <muellerobin95@gmail.com>
2023-07-18 22:09:05 +0200

    Add StatCard component

--- src/pfd-client/src/routes/Dashboard.tsx
+++ src/pfd-client/src/routes/Dashboard.tsx
@@ -1,5 +1,21 @@
+import StatsGrid, {
+  StatsGridProps,
+  icons,
+} from "../components/stat-card/StatCard";
+
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const data: StatsGridProps["data"] = [
+    { title: "Revenue", icon: "receipt", value: "13,456", diff: 34 },
+    { title: "Profit", icon: "coin", value: "4,145", diff: -13 },
+    { title: "Coupons usage", icon: "discount", value: "745", diff: 18 },
+    { title: "New customers", icon: "user", value: "188", diff: -30 },
+  ];
+
+  return (
+    <div>
+      <StatsGrid data={data}></StatsGrid>
+    </div>
+  );
 };
 
 export default Dashboard;
