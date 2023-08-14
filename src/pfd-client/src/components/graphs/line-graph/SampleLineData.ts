3bfa54da9a19e8e0dc5efb5ae429209d066587b1
RobinTTY <muellerobin95@gmail.com>
2023-08-14 22:55:59 +0200

    Add basic LineGraph

---
+++ src/pfd-client/src/components/graphs/line-graph/SampleLineData.ts
@@ -0,0 +1,272 @@
+export const SampleLineData = [
+  {
+    id: "japan",
+    color: "hsl(289, 70%, 50%)",
+    data: [
+      {
+        x: "plane",
+        y: 161,
+      },
+      {
+        x: "helicopter",
+        y: 51,
+      },
+      {
+        x: "boat",
+        y: 271,
+      },
+      {
+        x: "train",
+        y: 52,
+      },
+      {
+        x: "subway",
+        y: 0,
+      },
+      {
+        x: "bus",
+        y: 245,
+      },
+      {
+        x: "car",
+        y: 262,
+      },
+      {
+        x: "moto",
+        y: 297,
+      },
+      {
+        x: "bicycle",
+        y: 297,
+      },
+      {
+        x: "horse",
+        y: 109,
+      },
+      {
+        x: "skateboard",
+        y: 50,
+      },
+      {
+        x: "others",
+        y: 271,
+      },
+    ],
+  },
+  {
+    id: "france",
+    color: "hsl(306, 70%, 50%)",
+    data: [
+      {
+        x: "plane",
+        y: 43,
+      },
+      {
+        x: "helicopter",
+        y: 95,
+      },
+      {
+        x: "boat",
+        y: 219,
+      },
+      {
+        x: "train",
+        y: 252,
+      },
+      {
+        x: "subway",
+        y: 276,
+      },
+      {
+        x: "bus",
+        y: 205,
+      },
+      {
+        x: "car",
+        y: 33,
+      },
+      {
+        x: "moto",
+        y: 95,
+      },
+      {
+        x: "bicycle",
+        y: 57,
+      },
+      {
+        x: "horse",
+        y: 243,
+      },
+      {
+        x: "skateboard",
+        y: 230,
+      },
+      {
+        x: "others",
+        y: 156,
+      },
+    ],
+  },
+  {
+    id: "us",
+    color: "hsl(303, 70%, 50%)",
+    data: [
+      {
+        x: "plane",
+        y: 205,
+      },
+      {
+        x: "helicopter",
+        y: 118,
+      },
+      {
+        x: "boat",
+        y: 63,
+      },
+      {
+        x: "train",
+        y: 215,
+      },
+      {
+        x: "subway",
+        y: 13,
+      },
+      {
+        x: "bus",
+        y: 72,
+      },
+      {
+        x: "car",
+        y: 59,
+      },
+      {
+        x: "moto",
+        y: 103,
+      },
+      {
+        x: "bicycle",
+        y: 244,
+      },
+      {
+        x: "horse",
+        y: 248,
+      },
+      {
+        x: "skateboard",
+        y: 188,
+      },
+      {
+        x: "others",
+        y: 3,
+      },
+    ],
+  },
+  {
+    id: "germany",
+    color: "hsl(130, 70%, 50%)",
+    data: [
+      {
+        x: "plane",
+        y: 157,
+      },
+      {
+        x: "helicopter",
+        y: 21,
+      },
+      {
+        x: "boat",
+        y: 284,
+      },
+      {
+        x: "train",
+        y: 271,
+      },
+      {
+        x: "subway",
+        y: 199,
+      },
+      {
+        x: "bus",
+        y: 287,
+      },
+      {
+        x: "car",
+        y: 249,
+      },
+      {
+        x: "moto",
+        y: 90,
+      },
+      {
+        x: "bicycle",
+        y: 97,
+      },
+      {
+        x: "horse",
+        y: 94,
+      },
+      {
+        x: "skateboard",
+        y: 261,
+      },
+      {
+        x: "others",
+        y: 92,
+      },
+    ],
+  },
+  {
+    id: "norway",
+    color: "hsl(276, 70%, 50%)",
+    data: [
+      {
+        x: "plane",
+        y: 215,
+      },
+      {
+        x: "helicopter",
+        y: 22,
+      },
+      {
+        x: "boat",
+        y: 291,
+      },
+      {
+        x: "train",
+        y: 19,
+      },
+      {
+        x: "subway",
+        y: 42,
+      },
+      {
+        x: "bus",
+        y: 153,
+      },
+      {
+        x: "car",
+        y: 130,
+      },
+      {
+        x: "moto",
+        y: 292,
+      },
+      {
+        x: "bicycle",
+        y: 211,
+      },
+      {
+        x: "horse",
+        y: 240,
+      },
+      {
+        x: "skateboard",
+        y: 129,
+      },
+      {
+        x: "others",
+        y: 264,
+      },
+    ],
+  },
+];
--- src/pfd-client/src/routes/Accounts.tsx
+++ src/pfd-client/src/routes/Accounts.tsx
@@ -3,8 +3,10 @@ import { GetAccountsQuery } from "../queries/GetAccounts";
 import { Center, Loader, SimpleGrid } from "@mantine/core";
 import AccountSummary from "../components/account-summary/AccountSummary";
 import { AccountSummaryProps } from "../components/account-summary/AccountSummaryProps";
+import LineGraph from "../components/graphs/line-graph/LineGraph";
 
 // TODO: Add all accounts view?
+// TODO: Add icon (e.g. bank logo)
 const Accounts = () => {
   const { loading, error, data } = useQuery(GetAccountsQuery, {
     variables: { first: 5 },
@@ -14,7 +16,7 @@ const Accounts = () => {
   if (data)
     data.accounts.edges.map((account: any) => {
       accounts.push(account.node);
-      console.log(account.node);
+      // console.log(account.node);
     });
 
   if (loading)
@@ -25,11 +27,15 @@ const Accounts = () => {
     );
 
   return (
-    <SimpleGrid cols={2} breakpoints={[{ maxWidth: "xs", cols: 1 }]}>
-      {accounts.map((account: AccountSummaryProps) => {
-        return <AccountSummary {...account} />;
-      })}
-    </SimpleGrid>
+    <>
+      <SimpleGrid cols={2} breakpoints={[{ maxWidth: "xs", cols: 1 }]}>
+        {accounts.map((account: AccountSummaryProps) => {
+          // TODO: Add key
+          return <AccountSummary {...account} key={account.balance} />;
+        })}
+      </SimpleGrid>
+      <LineGraph />
+    </>
   );
 };
 
