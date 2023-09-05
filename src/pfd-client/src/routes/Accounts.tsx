9dfef412176d55c82c93a2f5aa788e415e57040e
RobinTTY <muellerobin95@gmail.com>
2023-09-05 22:02:36 +0200

    Add "Add Account" button + modal logic

--- src/pfd-client/src/routes/Accounts.tsx
+++ src/pfd-client/src/routes/Accounts.tsx
@@ -1,12 +1,16 @@
 import { useQuery } from "@apollo/client";
 import { GetAccountsQuery } from "../queries/GetAccounts";
-import { Center, Loader, SimpleGrid } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import { Modal, Center, Loader, SimpleGrid, Button } from "@mantine/core";
 import { AccountSummary } from "../components/account-summary/AccountSummary";
 import { AccountSummaryProps } from "../components/account-summary/AccountSummaryProps";
+
+import "./Accounts.css";
 
 // TODO: Add all accounts view?
 // TODO: Add icon (e.g. bank logo)
 export const Accounts = () => {
+  const [opened, { open, close }] = useDisclosure(false);
   const { loading, error, data } = useQuery(GetAccountsQuery, {
     variables: { first: 5 },
   });
@@ -25,14 +29,31 @@ export const Accounts = () => {
       </Center>
     );
 
+  // TODO: common css class for 100% height/width container?
   return (
     <>
-      <SimpleGrid mt="2%" cols={2} breakpoints={[{ maxWidth: "xs", cols: 1 }]}>
-        {accounts.map((account: AccountSummaryProps) => {
-          // TODO: Add key
-          return <AccountSummary {...account} key={account.balance} />;
-        })}
-      </SimpleGrid>
+      <Modal opened={opened} onClose={close} title="Authentication" centered>
+        test
+      </Modal>
+      <div className="accounts-container">
+        <div>
+          <SimpleGrid
+            mt="2%"
+            cols={2}
+            breakpoints={[{ maxWidth: "xs", cols: 1 }]}
+          >
+            {accounts.map((account: AccountSummaryProps) => {
+              // TODO: Add key
+              return <AccountSummary {...account} key={account.balance} />;
+            })}
+          </SimpleGrid>
+        </div>
+        <Center py="md">
+          <Button onClick={open} size="md">
+            Add Account
+          </Button>
+        </Center>
+      </div>
     </>
   );
 };
