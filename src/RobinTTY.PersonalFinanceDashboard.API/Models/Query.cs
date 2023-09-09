7cf0cac3340313e0f83c6628d813b8ad9592553d
RobinTTY <muellerobin95@gmail.com>
2023-09-09 05:44:45 +0200

    Expose AuthenticationRequest data via graphql API

--- src/RobinTTY.PersonalFinanceDashboard.API/Models/Query.cs
+++ src/RobinTTY.PersonalFinanceDashboard.API/Models/Query.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq;
+using System.Threading.Tasks;
 using HotChocolate.Types;
 using RobinTTY.PersonalFinanceDashboard.Core.Models;
 using RobinTTY.PersonalFinanceDashboard.Database.Mock;
@@ -27,4 +28,27 @@ public class Query
     // TODO: Investigate how to handle the return type (result/error) properly with paging
     [UsePaging]
     public IQueryable<BankingInstitution> GetBankingInstitutions() => _dataProvider.GetBankingInstitutions().GetAwaiter().GetResult().Result!;
+
+    /// <summary>
+    /// TODO
+    /// </summary>
+    /// <returns></returns>
+    public async Task<IQueryable<AuthenticationRequest>> GetAuthenticationRequests()
+    {
+        // TODO: Limit?
+        var requests = await _dataProvider.GetAuthenticationRequests(100);
+        return requests.Result!.AsQueryable();
+    }
+
+    /// <summary>
+    /// TODO
+    /// </summary>
+    /// <param name="institutionId"></param>
+    /// <param name="redirectUri"></param>
+    /// <returns></returns>
+    public async Task<AuthenticationRequest> GetNewAuthenticationRequest(string institutionId, string redirectUri)
+    {
+        var request = await _dataProvider.GetNewAuthenticationRequest(institutionId, new Uri(redirectUri));
+        return request.Result!;
+    }
 }
