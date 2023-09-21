91b812ec50e8ff4441cc4a69586f559b74f52534
RobinTTY <muellerobin95@gmail.com>
2023-09-22 01:45:08 +0200

    Migrate Dashboard to Mantine v7

--- src/pfd-client/src/routes/Dashboard.tsx
+++ src/pfd-client/src/routes/Dashboard.tsx
@@ -1,10 +1,10 @@
-import { Loader, Center } from "@mantine/core";
-import { useQuery } from "@apollo/client";
+import { Loader, Center, Space } from '@mantine/core';
+import { useQuery } from '@apollo/client';
 
-import { StatCardProps } from "../components/stat-card/StatCardProps";
-import { GetAccountsQuery } from "../queries/GetAccounts";
-import { LineGraph } from "../components/graphs/line-graph/LineGraph";
-import { StatsGrid } from "../components/stat-grid/StatsGrid";
+import { StatCardProps } from '../components/stat-card/StatCardProps';
+import { GetAccountsQuery } from '../queries/GetAccounts';
+import { LineGraph } from '../components/graphs/line-graph/LineGraph';
+import { StatsGrid } from '../components/stat-grid/StatsGrid';
 
 export const Dashboard = () => {
   const { loading, error, data } = useQuery(GetAccountsQuery, {
@@ -14,19 +14,19 @@ export const Dashboard = () => {
   // TODO: Remove this duplicate code (also used in Accounts)
   if (loading)
     return (
-      <Center h={"100%"}>
+      <Center h={'100%'}>
         <Loader color="violet" />
       </Center>
     );
 
   const gridprops: StatCardProps[] = [
-    { title: "Net Worth", icon: "coin", value: "$13,456", diff: 34 },
-    { title: "Profit", icon: "receipt", value: "$145.56", diff: -13 },
-    { title: "Transactions", icon: "discount", value: "23", diff: 8 },
+    { title: 'Net Worth', icon: 'coin', value: '$13,456', diff: 34 },
+    { title: 'Profit', icon: 'receipt', value: '$145.56', diff: -13 },
+    { title: 'Transactions', icon: 'discount', value: '23', diff: 8 },
     {
-      title: "Followers",
-      icon: "user",
-      value: "1835",
+      title: 'Followers',
+      icon: 'user',
+      value: '1835',
       diff: 3,
     },
   ];
@@ -34,6 +34,7 @@ export const Dashboard = () => {
   return (
     <>
       <LineGraph />
+      <Space h="md" />
       <StatsGrid data={gridprops}></StatsGrid>
     </>
   );
