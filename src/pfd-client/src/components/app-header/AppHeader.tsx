b537cc38c800f4703fa32f5ca585ba5e8cfd41c2
RobinTTY <muellerobin95@gmail.com>
2023-09-21 23:41:34 +0200

    Migrate AppHeader to Mantine v7

--- src/pfd-client/src/components/app-header/AppHeader.tsx
+++ src/pfd-client/src/components/app-header/AppHeader.tsx
@@ -1,23 +1,15 @@
-import { Group, Text, ActionIcon, useMantineColorScheme } from "@mantine/core";
-import { IconSun, IconMoonStars } from "@tabler/icons-react";
+import { Group, Text, ActionIcon, useMantineColorScheme } from '@mantine/core';
+import { IconSun, IconMoonStars } from '@tabler/icons-react';
 
 export const AppHeader = () => {
   const { colorScheme, toggleColorScheme } = useMantineColorScheme();
 
   // TODO: reconsider spacing units (rem, 40...)
   return (
-    <Group position="apart" style={{ width: "100%" }}>
+    <Group justify="space-between" style={{ width: '100%' }}>
       <Text>Personal Finance Dashboard</Text>
-      <ActionIcon
-        variant="default"
-        onClick={() => toggleColorScheme()}
-        size={35}
-      >
-        {colorScheme === "dark" ? (
-          <IconSun size="1.2rem" />
-        ) : (
-          <IconMoonStars size="1.2rem" />
-        )}
+      <ActionIcon variant="default" onClick={() => toggleColorScheme()} size={35}>
+        {colorScheme === 'dark' ? <IconSun size="1.2rem" /> : <IconMoonStars size="1.2rem" />}
       </ActionIcon>
     </Group>
   );
