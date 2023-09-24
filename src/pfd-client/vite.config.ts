449565060b13b4dc4f554c87f5db5661f1281aba
RobinTTY <muellerobin95@gmail.com>
2023-09-24 17:26:45 +0200

    Add typescript, eslint checks to vite workflow

--- src/pfd-client/vite.config.ts
+++ src/pfd-client/vite.config.ts
@@ -1,10 +1,11 @@
 import * as path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import checker from 'vite-plugin-checker';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), checker({ typescript: true, eslint: { lintCommand: 'eslint src' } })],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
