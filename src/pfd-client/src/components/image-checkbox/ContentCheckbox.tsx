0414bbb49bd42d25e0f186a554c0448bc85c1ceb
RobinTTY <muellerobin95@gmail.com>
2023-09-25 17:35:54 +0200

    Convert ContentCheckbox to a controlled component

--- src/pfd-client/src/components/image-checkbox/ContentCheckbox.tsx
+++ src/pfd-client/src/components/image-checkbox/ContentCheckbox.tsx
@@ -1,43 +1,30 @@
-import { PropsWithChildren } from 'react';
 import { UnstyledButton, Checkbox, Box } from '@mantine/core';
-import { useUncontrolled } from '@mantine/hooks';
 import { ContentCheckboxProps } from './ContentCheckboxProps';
 
 // TODO: remove unnecessary css
 import classes from './ContentCheckbox.module.css';
 
+// TODO: Can this be simplified so onChange is not necessary?
 export const ContentCheckbox = ({
-  checked,
-  defaultChecked,
-  onChange,
   className,
   children,
+  checkboxProps,
+  onChange,
   ...others
-}: PropsWithChildren<ContentCheckboxProps> &
-  Omit<React.ComponentPropsWithoutRef<'button'>, keyof ContentCheckboxProps>) => {
-  const [value, handleChange] = useUncontrolled({
-    value: checked,
-    defaultValue: defaultChecked,
-    finalValue: false,
-    onChange,
-  });
-
-  return (
-    <UnstyledButton
-      {...others}
-      onClick={() => handleChange(!value)}
-      data-checked={value || undefined}
-      className={classes.button}
-    >
-      <Box className={classes.body}>{children}</Box>
-      <Checkbox
-        checked={value}
-        onChange={() => {}}
-        tabIndex={-1}
-        size="md"
-        styles={{ input: { cursor: 'pointer' } }}
-        pl="xl"
-      />
-    </UnstyledButton>
-  );
-};
+}: ContentCheckboxProps) => (
+  <UnstyledButton
+    onClick={() => onChange?.(!checkboxProps.checked)}
+    data-checked={checkboxProps.checked || undefined}
+    className={classes.button}
+    {...others}
+  >
+    <Box className={classes.body}>{children}</Box>
+    <Checkbox
+      styles={{ input: { cursor: 'pointer' } }}
+      tabIndex={-1}
+      size="md"
+      pl="xl"
+      {...checkboxProps}
+    />
+  </UnstyledButton>
+);
--- src/pfd-client/src/components/image-checkbox/ContentCheckboxProps.ts
+++ src/pfd-client/src/components/image-checkbox/ContentCheckboxProps.ts
@@ -1,5 +1,10 @@
-export interface ContentCheckboxProps {
-  checked?: boolean;
-  defaultChecked?: boolean;
+import { PropsWithChildren } from 'react';
+import { CheckboxProps } from '@mantine/core';
+
+export interface PartialContentCheckboxProps {
+  checkboxProps: CheckboxProps;
   onChange?(checked: boolean): void;
 }
+
+export type ContentCheckboxProps = PropsWithChildren<PartialContentCheckboxProps> &
+  Omit<React.ComponentPropsWithoutRef<'button'>, keyof PartialContentCheckboxProps>;
