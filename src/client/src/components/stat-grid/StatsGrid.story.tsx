11e3067afc7c44b72a94ac6a2d08f5f575530db9
RobinTTY <muellerobin95@gmail.com>
2023-10-05 19:04:57 +0200

    Add StatsGrid story

---
+++ src/client/src/components/stat-grid/StatsGrid.story.tsx
@@ -0,0 +1,29 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { StatsGrid } from './StatsGrid';
+import { StatCardProps } from '../stat-card/StatCardProps';
+
+const meta: Meta<typeof StatsGrid> = {
+  component: StatsGrid,
+  title: 'Components/StatsGrid',
+  tags: ['autodocs'],
+};
+
+type Story = StoryObj<typeof StatsGrid>;
+
+const gridprops: StatCardProps[] = [
+  { title: 'Net Worth', icon: 'coin', value: '$13,456', diff: 34 },
+  { title: 'Profit', icon: 'receipt', value: '$145.56', diff: -13 },
+  { title: 'Transactions', icon: 'discount', value: '23', diff: 8 },
+  {
+    title: 'Followers',
+    icon: 'user',
+    value: '1835',
+    diff: 3,
+  },
+];
+
+export const Default: Story = {
+  render: () => <StatsGrid data={gridprops} />,
+};
+
+export default meta;
--- src/client/src/components/stat-grid/StatsGrid.tsx
+++ src/client/src/components/stat-grid/StatsGrid.tsx
@@ -3,6 +3,7 @@ import { SimpleGrid } from '@mantine/core';
 import { StatCard } from '@components/stat-card/StatCard';
 import { StatsGridProps } from './StatsGridProps';
 
+// TODO: Should this even be a component? Probably not.
 // TODO: should use children
 export const StatsGrid = (props: StatsGridProps) => (
   <SimpleGrid cols={{ xs: 1, md: 2, lg: 4 }}>
