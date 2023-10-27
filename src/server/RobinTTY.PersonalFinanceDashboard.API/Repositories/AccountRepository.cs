505277f9cc845b84e5b598aa0fa00dcdc006ad50
RobinTTY <muellerobin95@gmail.com>
2023-10-27 02:10:20 +0200

    Add AccountRepository

---
+++ src/server/RobinTTY.PersonalFinanceDashboard.API/Repositories/AccountRepository.cs
@@ -0,0 +1,31 @@
+﻿using RobinTTY.PersonalFinanceDashboard.Core.Models;
+using RobinTTY.PersonalFinanceDashboard.ThirdPartyDataProviders;
+
+namespace RobinTTY.PersonalFinanceDashboard.API.Repositories;
+
+/// <summary>
+/// Manages <see cref="Account"/> data retrieval.
+/// </summary>
+public class AccountRepository
+{
+    private readonly ApplicationDbContext _dbContext;
+    private readonly GoCardlessDataProvider _dataProvider;
+
+    public AccountRepository(ApplicationDbContext dbContext, GoCardlessDataProvider dataProvider)
+    {
+        _dbContext = dbContext;
+        _dataProvider = dataProvider;
+    }
+
+    public async Task<BankAccount?> Get(string accountId)
+    {
+        var account = await _dataProvider.GetBankAccount(accountId);
+        return account.Result;
+    }
+
+    public async Task<IEnumerable<BankAccount>> Get(IEnumerable<string> accountIds)
+    {
+        var accounts = await _dataProvider.GetBankAccounts(accountIds);
+        return accounts.Select(query => query.Result)!;
+    }
+}
--- src/server/RobinTTY.PersonalFinanceDashboard.API/Types/Queries/AccountQueryResolvers.cs
+++ src/server/RobinTTY.PersonalFinanceDashboard.API/Types/Queries/AccountQueryResolvers.cs
@@ -1,7 +1,6 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections;
 using HotChocolate.Types;
+using RobinTTY.PersonalFinanceDashboard.API.Repositories;
 using RobinTTY.PersonalFinanceDashboard.Core.Models;
 using RobinTTY.PersonalFinanceDashboard.ThirdPartyDataProviders;
 
@@ -10,32 +9,25 @@ namespace RobinTTY.PersonalFinanceDashboard.Api.Types.Queries;
 [QueryType]
 public sealed class AccountQueryResolvers
 {
-    private readonly GoCardlessDataProvider _dataProvider;
-
-    public AccountQueryResolvers(GoCardlessDataProvider dataProvider)
-    {
-        _dataProvider = dataProvider;
-    }
-
     // TODO: probably should be GetBankAccount and then separate route for investment accounts
     /// <summary>
     /// Look up an account by its id.
     /// </summary>
     /// <param name="accountId">The id of the account to lookup.</param>
-    public async Task<BankAccount?> GetAccount(string accountId)
+    /// <param name="repository">The repository to use for data retrieval.</param>
+    public async Task<BankAccount?> GetAccount(string accountId, AccountRepository repository)
     {
-        var account = await _dataProvider.GetBankAccount(accountId);
-        return account.Result;
+        return await repository.Get(accountId);
     }
 
     /// <summary>
     /// Look up accounts by a list of ids.
     /// </summary>
     /// <param name="accountIds">The ids of the accounts to retrieve.</param>
+    /// <param name="repository">The repository to use for data retrieval.</param>
     [UsePaging]
-    public async Task<IQueryable<BankAccount>> GetAccounts(IEnumerable<string> accountIds)
+    public async Task<IEnumerable<BankAccount>> GetAccounts(IEnumerable<string> accountIds, AccountRepository repository)
     {
-        var accounts = await _dataProvider.GetBankAccounts(accountIds);
-        return accounts.Select(query => query.Result).AsQueryable()!;
+        return await repository.Get(accountIds);
     }
 }
