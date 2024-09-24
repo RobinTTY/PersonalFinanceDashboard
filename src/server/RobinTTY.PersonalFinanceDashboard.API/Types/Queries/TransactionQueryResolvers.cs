b135b454c156faf6f6e4ef7e68d4134cfa7f4d7c
RobinTTY <muellerobin95@gmail.com>
2024-09-25 01:15:24 +0200

    Update TransactionQueryResolvers.cs documentation

--- src/server/RobinTTY.PersonalFinanceDashboard.API/Types/Queries/TransactionQueryResolvers.cs
+++ src/server/RobinTTY.PersonalFinanceDashboard.API/Types/Queries/TransactionQueryResolvers.cs
@@ -10,17 +10,23 @@ namespace RobinTTY.PersonalFinanceDashboard.Api.Types.Queries;
 [QueryType]
 public class TransactionQueryResolvers
 {
+    /// <summary>
+    /// Look up transactions.
+    /// </summary>
+    /// <param name="repository">The repository to use for data retrieval.</param>
+    /// <param name="cancellationToken">Optional token to signal cancellation of the operation.</param>
     public async Task<IEnumerable<Transaction>> GetTransactions(TransactionRepository repository,
         CancellationToken cancellationToken)
     {
         return await repository.GetAll(cancellationToken);
     }
-    
+
     /// <summary>
     /// Look up transactions of an account.
     /// </summary>
     /// <param name="accountId">The id of the account to retrieve.</param>
     /// <param name="repository">The repository to use for data retrieval.</param>
+    /// <param name="cancellationToken">Optional token to signal cancellation of the operation.</param>
     [UsePaging]
     public async Task<IEnumerable<Transaction>> GetTransactionsByAccountId(string accountId, TransactionRepository repository, CancellationToken cancellationToken)
     {
