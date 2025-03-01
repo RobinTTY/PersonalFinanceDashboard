c0a106bea86b47d53d701e37b7bc3e99705b8739
RobinTTY <muellerobin95@gmail.com>
2025-03-01 16:47:35 +0100

    Make ThirdPartyEntity an index

---
+++ src/server/RobinTTY.PersonalFinanceDashboard.Infrastructure/EntityConfigurations/ThirdPartyEntityTypeConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using RobinTTY.PersonalFinanceDashboard.Core.Models.Base;
+
+namespace RobinTTY.PersonalFinanceDashboard.Infrastructure.EntityConfigurations;
+
+public class ThirdPartyEntityTypeConfiguration : IEntityTypeConfiguration<ThirdPartyEntity>
+{
+    public void Configure(EntityTypeBuilder<ThirdPartyEntity> builder)
+    {
+        builder.HasIndex(entity => entity.ThirdPartyId);
+    }
+}
