0de03e0f56d8428f15e12be72b1372f7eb62ed12
RobinTTY <muellerobin95@gmail.com>
2025-08-09 11:46:38 +0200

    Upgrade docusaurus version

--- docs/docusaurus/docusaurus.config.js
+++ docs/docusaurus/docusaurus.config.js
@@ -1,6 +1,6 @@
 // @ts-check
-const lightCodeTheme = require("prism-react-renderer/themes/github");
-const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+const lightCodeTheme = require("prism-react-renderer").themes.github;
+const darkCodeTheme = require("prism-react-renderer").themes.dracula;
 const projectTitle = "Personal Finance Dashboard";
 
 /** @type {import('@docusaurus/types').Config} */
