37cf188ec53bd2935c3da4b67473a19ac8c8a709
RobinTTY <muellerobin95@gmail.com>
2025-12-21 19:36:25 +0100

    Remove unused classes

--- src/server/RobinTTY.PersonalFinanceDashboard.Infrastructure/Models/DataSyncDependency.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace RobinTTY.PersonalFinanceDashboard.Infrastructure.Models;
-
-public class DataSyncDependency
-{
-    public SyncDataType DataType { get; set; }
-    public List<SyncDataType> Dependencies { get; set; } = new();
-}
