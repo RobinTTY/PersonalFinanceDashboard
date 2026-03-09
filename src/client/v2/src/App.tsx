74935f984f839c8203d4dc5e1a7b542d4d244d57
RobinTTY <muellerobin95@gmail.com>
2026-03-09 02:21:09 +0100

    feat: integrate apollo graphql client

--- src/client/v2/src/App.tsx
+++ src/client/v2/src/App.tsx
@@ -1,13 +1,22 @@
 import '@mantine/core/styles.css';
 
+import { ApolloClient, HttpLink, InMemoryCache, gql } from "@apollo/client";
+import { ApolloProvider } from "@apollo/client/react";
 import { MantineProvider } from '@mantine/core';
 import { Router } from './Router';
 import { theme } from './theme';
 
+const client = new ApolloClient({
+  link: new HttpLink({ uri: "http://localhost:5115/graphql" }),
+  cache: new InMemoryCache(),
+});
+
 export default function App() {
   return (
-    <MantineProvider theme={theme} defaultColorScheme="auto">
-      <Router />
-    </MantineProvider>
+    <ApolloProvider client={client}>
+      <MantineProvider theme={theme} defaultColorScheme="auto">
+        <Router />
+      </MantineProvider>
+    </ApolloProvider>
   );
 }
