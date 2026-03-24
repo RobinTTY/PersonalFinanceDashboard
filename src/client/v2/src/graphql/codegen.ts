e9cde5663047a53c47e5e3ce4428db397bcd5401
RobinTTY <muellerobin95@gmail.com>
2026-03-24 02:56:37 +0100

    chore: refactor comments in graphql CodegenConfig

--- src/client/v2/src/graphql/codegen.ts
+++ src/client/v2/src/graphql/codegen.ts
@@ -10,22 +10,31 @@ const config: CodegenConfig = {
   documents: ["src/**/*.{ts,tsx}"],
   ignoreNoDocuments: true,
   generates: {
-    // Use a path that works the best for the structure of your application
     "src/graphql/types/graphql.ts": {
       plugins: ["typescript", "typescript-operations"],
       config: {
         avoidOptionals: {
-          // Use `null` for nullable fields instead of optionals
+            /**
+             * Use `null` for nullable fields instead of optionals
+             */
           field: true,
-          // Allow nullable input fields to remain unspecified
+          /**
+           * Allow nullable input fields to remain unspecified
+           */
           inputValue: false,
         },
-        // Use `unknown` instead of `any` for unconfigured scalars
+        /**
+         * Use `unknown` instead of `any` for non-configured scalars
+         */
         defaultScalarType: "unknown",
-        // Always includes `__typename` fields
+        /**
+         * Always includes `__typename` fields
+         */
         nonOptionalTypename: true,
-        // Apollo Client doesn't add the `__typename` field to root types so
-        // don't generate a type for the `__typename` for root operation types.
+        /**
+         * Apollo Client doesn't add the `__typename` field to root types so
+         * don't generate a type for the `__typename` for root operation types.
+         */
         skipTypeNameForRoot: true,
       },
     },
